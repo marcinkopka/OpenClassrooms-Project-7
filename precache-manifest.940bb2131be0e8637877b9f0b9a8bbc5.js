273c0856e03d1e2691c42b8f6d0f5b6f4fb13c23
MarcinKopka <marcinkopka79@gmail.com>
2019-10-15 18:58:02 +0100

    Updates

---
+++ precache-manifest.940bb2131be0e8637877b9f0b9a8bbc5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "631542a7f8b2d96ad0def5b47299e009",
+    "url": "/OpenClassrooms-Project-7/static/media/utensils-solid.631542a7.svg"
+  },
+  {
+    "revision": "550b0f9205116115959778c0388d6ce0",
+    "url": "/OpenClassrooms-Project-7/static/media/times-circle-regular.550b0f92.svg"
+  },
+  {
+    "revision": "daf00eadae78d83f803affa30c851abd",
+    "url": "/OpenClassrooms-Project-7/static/media/phone-solid.daf00ead.svg"
+  },
+  {
+    "revision": "c8fed0b3f607c90cf61883399fbe2e21",
+    "url": "/OpenClassrooms-Project-7/static/media/map-marker-alt-solid.c8fed0b3.svg"
+  },
+  {
+    "revision": "a7dcae6fe545a03922da2964e3821c48",
+    "url": "/OpenClassrooms-Project-7/static/media/at-solid.a7dcae6f.svg"
+  },
+  {
+    "revision": "3ab13a41d457432716cb",
+    "url": "/OpenClassrooms-Project-7/static/js/runtime~main.eecb71e8.js"
+  },
+  {
+    "revision": "89ad11dd9aa8261a914d",
+    "url": "/OpenClassrooms-Project-7/static/js/main.7ebc0898.chunk.js"
+  },
+  {
+    "revision": "554dacc80dbbfffccb7a",
+    "url": "/OpenClassrooms-Project-7/static/js/2.dda64f3f.chunk.js"
+  },
+  {
+    "revision": "89ad11dd9aa8261a914d",
+    "url": "/OpenClassrooms-Project-7/static/css/main.63eb910d.chunk.css"
+  },
+  {
+    "revision": "cfe288549ad8f4888aa1411b221ea417",
+    "url": "/OpenClassrooms-Project-7/index.html"
+  }
+];
