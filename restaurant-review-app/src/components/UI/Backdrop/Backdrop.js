96a346b65b3a8fb44ce90b8b8ee0dc5ce62b6fdf
MarcinKopka <marcinkopka79@gmail.com>
2022-04-27 17:43:38 +0100

    Project files removed.

--- restaurant-review-app/src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Backdrop.module.css';
-
-const backdrop = (props) => (
-  props.addingRestaurantStatus ? <div className={classes.Backdrop} onClick={props.addingRestaurantHideHandler}></div> : null
-);
-
-export default backdrop;
